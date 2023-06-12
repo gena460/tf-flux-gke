2ce988fca9ffde856ae328085e932cf61d0fb021
gena460 <gena460@gmail.com>
2023-06-12 22:17:10 +0300

    delete spaces

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "flux_bootstrap" {
   source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
-  #  config_path       = module.gke_cluster.kubeconfig
+  #config_path       = module.gke_cluster.kubeconfig
   github_token = var.GITHUB_TOKEN
   config_host  = module.gke_cluster.config_host
   config_token = module.gke_cluster.config_token
