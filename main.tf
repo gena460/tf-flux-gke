17cf792505a2b5fa8122ef45879e6a6daa519711
gena460 <gena460@gmail.com>
2023-06-17 01:38:55 +0300

    Add WI and KMS

--- main.tf
+++ main.tf
@@ -30,6 +30,27 @@ module "tls_private_key" {
   algorithm = var.algorithm
 }
 
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/den-vasyliev/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
+}
+
 # module "kind_cluster" {
 #   source       = "github.com/den-vasyliev/tf-kind-cluster"
 #   cluster_name = "kind-cluster"
